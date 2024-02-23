76a21f4da2bdbb3636f9543227e6854d2a11f8dc
marciodutra <78557074+marciodutra@users.noreply.github.com>
2024-02-23 16:34:50 -0300

    Add files via upload

--- cadastro_script.php
+++ cadastro_script.php
@@ -5,6 +5,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Cadastro</title>
     <link rel="stylesheet" href="estilo.css"/>
     <link href="css/estilo.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -23,19 +24,12 @@
             $telefone = $_POST['telefone'];
             $email = $_POST['email'];
             $data_nascimento = $_POST['data_nascimento'];
-            $foto = $_FILES['foto'];
-            $nome_foto = mover_foto($foto);
-            if($nome_foto == 0) {
-              $nome_foto = null;
-            }
+            
 
-            $sql = "INSERT INTO `pessoas`(`nome`, `endereco`, `telefone`, `email`, `data_nascimento`, `foto`) VALUES ('$nome','$endereco',
-            '$telefone','$email','$data_nascimento', '$nome_foto')";
+            $sql = "INSERT INTO `pessoas`(`nome`, `endereco`, `telefone`, `email`, `data_nascimento`) VALUES ('$nome','$endereco',
+            '$telefone','$email','$data_nascimento')";
 
             if(mysqli_query($conn, $sql)) {
-              if($nome_foto != null) {
-                echo "<img src='img/$nome_foto' title='$nome_foto' class='mostra_foto'>";
-              }
                 mensagem("$nome cadastrado com sucesso!", 'success');
             }else {
                 mensagem("$nome não foi cadastrado", 'danger');
--- edit_script.php
+++ edit_script.php
@@ -5,6 +5,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Alteração de Cadastro</title>
     <link href="restrito/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
